d320851600ada9d998cc982ebbd2fcd0cc0d9ba6
karar rifat <kararrifat@gmail.com>
2022-12-18 03:15:38 +0600

    27

---
+++ 027_Remove_Element.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution_27{
+    public int removeElement(int[] nums, int val) {
+        int c = 0;
+        int length = nums.length;
+
+        for (int i=0; i<length; i++){
+            if(nums[i] != val){
+                nums[c++] = nums[i];
+            }
+        }
+        return c;
+    }
+
+//    [0,1,2,2,3,0,4,2] || val= 2
+//
+    public static void main(String[] args){
+        Solution_27 s = new Solution_27();
+        System.out.println(
+                s.removeElement(new int[] {0,1,2,2,3,0,4,2}, 2)
+        );
+    }
+}
