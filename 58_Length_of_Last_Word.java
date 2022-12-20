cc78557a88e59f7f978cdc8ebad9487f6e5d64a7
karar rifat <kararrifat@gmail.com>
2022-12-20 15:16:42 +0600

    58

---
+++ 58_Length_of_Last_Word.java
@@ -0,0 +1,32 @@
+class Solution_58{
+
+    // start from last of string. if space found before finding any character loop should continue.
+    // if space found after finding a char then surely a word is found
+    // program does work if there is no space meaning only one word is in the string
+    public int lengthOfLastWord(String s) {
+        char[] sentence = s.toCharArray();
+        int endPoint = sentence.length-1;
+        int counter = 0;
+        boolean shouldContinue = true;
+
+        while(endPoint>=0){
+            if(sentence[endPoint]==' '){
+                if(!shouldContinue){
+                    return counter;
+                }
+            }else {
+                shouldContinue = false;
+                counter++;
+            }
+            endPoint--;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args){
+        Solution_58 s = new Solution_58();
+        System.out.println(
+                s.lengthOfLastWord("   fly me   to   the moon  ")
+        );
+    }
+}
