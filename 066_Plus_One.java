b055413c96ef74b753a3edb1a1d4a1bea459d065
karar rifat <kararrifat@gmail.com>
2022-12-22 15:13:20 +0600

    66

--- 066_Plus_One.java
+++ 066_Plus_One.java
@@ -1,30 +1,33 @@
 class Solution_66{
-
-    // not completed
     public int[] plusOne(int[] digits) {
-        int carry = 0;
         int endPoint = digits.length-1;
-        if(endPoint != 9){
+        if(digits[endPoint] != 9){
             digits[endPoint] = digits[endPoint] + 1;
             return digits;
         }else {
-            carry = 1;
-            while(carry>0){
+            while(endPoint>=0 && digits[endPoint] == 9){
                 digits[endPoint] = 0;
                 endPoint--;
-                carry = 1;
+            }
+            if(digits[0] == 0){
+                int[] newArray = new int[digits.length + 1];
+                newArray[0] = 1;
+                return newArray;
+            }else {
+                digits[endPoint] = digits[endPoint] + 1;
             }
 
         }
-        return new int[] {1,2,3};
+        return digits;
+
     }
 
     public static void main(String[] args){
         Solution_66 s = new Solution_66();
-        int[] digits = {1,2,3};
+        int[] digits = {9,9};
 
-        s.plusOne(digits);
-        for(int i: digits){
+        int[] returned = s.plusOne(digits);
+        for(int i: returned){
             System.out.println(i);
         }
     }
