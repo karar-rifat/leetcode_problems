ad01016019957d5fea074e2b0e2d45a663d0b143
karar rifat <kararrifat@gmail.com>
2022-12-22 19:42:04 +0600

    69

---
+++ 069_Sqrt(x).java
@@ -0,0 +1,18 @@
+class Solution_69{
+    public int mySqrt(int x) {
+        long counter = 0;
+        if(x == 0) return 0;
+        while(counter*counter <= x){
+            counter++;
+        }
+        return (int)--counter ;
+    }
+
+    public static void main(String[] args) {
+        Solution_69 s = new Solution_69();
+        System.out.println(
+                s.mySqrt(2147395600)
+//                Integer.MAX_VALUE
+        );
+    }
+}
